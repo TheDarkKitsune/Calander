34a455b77b9526b711683c4ce05c3929784980f5
LYIVX <56669739+LYIVX@users.noreply.github.com>
2026-02-28 00:28:00 +0000

    moreee

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.1.0"),
+        .package(name: "CapacitorApp", path: "..\..\..\..\node_modules\.pnpm\@capacitor+app@8.0.1_@capacitor+core@8.1.0\node_modules\@capacitor\app"),
         .package(name: "CapacitorLocalNotifications", path: "..\..\..\..\node_modules\.pnpm\@capacitor+local-notifications@8.0.1_@capacitor+core@8.1.0\node_modules\@capacitor\local-notifications"),
         .package(name: "CapacitorPushNotifications", path: "..\..\..\..\node_modules\.pnpm\@capacitor+push-notifications@8.0.1_@capacitor+core@8.1.0\node_modules\@capacitor\push-notifications")
     ],
@@ -21,6 +22,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapacitorApp", package: "CapacitorApp"),
                 .product(name: "CapacitorLocalNotifications", package: "CapacitorLocalNotifications"),
                 .product(name: "CapacitorPushNotifications", package: "CapacitorPushNotifications")
             ]
